bc42f1c89393233a1591d41e45c08b6bd5f9f26d
rubenhortas <rubenhortas@gmail.com>
2019-06-26 20:17:51 +0200

    Added

---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                s1.SayHelloWorld();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void SayHelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
