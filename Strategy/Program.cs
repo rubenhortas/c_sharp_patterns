682f31d315864ab79366ca73a12fea11a389be26
rubenhortas <rubenhortas@gmail.com>
2022-07-14 20:38:03 +0200

    Refactored

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -4,27 +4,36 @@ namespace Strategy
 {
     class Program
     {
+        private const int TotalSale = 100;
+
         static void Main()
         {
-            Sale sale;
-            int totalSale = 100;
+            ApplyPercentageDiscount();
 
+            Console.WriteLine();
+
+            ApplyAbsoluteDiscountOverThreshold();
+        }
+
+        private static void ApplyPercentageDiscount()
+        {
             Console.WriteLine("Strategy Set Pricing Percentage Discount");
             Console.WriteLine($"{Constants.DiscountRate}% discount");
 
             IStrategySetSalePrice strategySetPricingPercentageDiscount = new StrategySetPricingPercentageDiscount();
+            Sale sale = new(TotalSale, strategySetPricingPercentageDiscount);
 
-            sale = new Sale(totalSale, strategySetPricingPercentageDiscount);
             sale.GetTotal();
+        }
 
-            Console.WriteLine();
-
+        private static void ApplyAbsoluteDiscountOverThreshold()
+        {
             Console.WriteLine("Strategy Set Pricing Absolute Discount Over Threshold");
             Console.WriteLine($"{Constants.Discount}€ discount (minimum sale of {Constants.SpendingThreshold}€)");
 
             IStrategySetSalePrice strategySetPricingAbsoluteDiscountOverThreshold = new StrategySetPricingAbsoluteDiscountOverThreshold();
+            Sale sale = new(TotalSale, strategySetPricingAbsoluteDiscountOverThreshold);
 
-            sale = new Sale(totalSale, strategySetPricingAbsoluteDiscountOverThreshold);
             sale.GetTotal();
         }
     }
