5b6631a99de967e8cf244426db713728a94c2b8c
rubenhortas <rubenhortas@gmail.com>
2022-07-14 20:27:58 +0200

    Deleted file

--- Factory/Doc/FactoryPattern.cs
+++
@@ -1,3 +0,0 @@
-﻿# Factory Pattern
-
-The factory design pattern is used to replace class constructors, abstracting the process of object generation so that the type of the object instantiated can be determined at run-time.
